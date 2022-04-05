4414fd49f406d3760dece9663cbd0f881cafd946
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-04-05 14:15:59 +0200

    fix: Expose Configuration type

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { Alice } from './characters/alice';
 export { Bob, RemoteBob } from './characters/bob';
 export { Enrico } from './characters/enrico';
 export { BlockchainPolicyParameters, EnactedPolicy } from './policies/policy';
+export { Configuration } from './types';
 
 export { PublicKey, MessageKit } from '@nucypher/nucypher-core';

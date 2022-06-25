e47e13bd1228cea62fedac4c945a9939bbb0ce0c
James Campbell <james.campbell@tanti.org.uk>
2022-06-25 17:22:00 -0400

    Include simple config structure for a tDec scheme

--- src/characters/tDec.ts
+++ src/characters/tDec.ts
@@ -1,14 +1,31 @@
-import { Bob } from './bob';
-import { Alice } from './alice';
-import { SecretKey } from '@nucypher/nucypher-core';
-import type { Web3Provider } from '@ethersproject/providers'
+import { tDecDecrypter } from './universal_bob';
+import { Enrico } from './enrico'
+import {
+  EncryptedTreasureMap,
+  PublicKey,
+  SecretKey,
+} from '@nucypher/nucypher-core';
 
-export const makeTDecEncrypter = (provider: Web3Provider, porterUri: string): Alice => {
-    const secretKey = SecretKey.fromBytes(Buffer.from('God Alice secret key'))
-    return Alice.fromSecretKey({ porterUri }, secretKey, provider)
+const tDecConfig : { [key: string]: { [key: string] : any } } = {
+  "simple": {
+    "policyEncryptingKey": PublicKey.fromBytes(),
+    "encryptedTreasureMap": EncryptedTreasureMap.fromBytes(),
+    "publisherVerifyingKey": PublicKey.fromBytes(),
+    "decryptingKey": SecretKey.fromBytes()
   }
+};
 
-  export const makeTDecDecrypter = (porterUri: string): Bob => {
-    const secretKey = SecretKey.fromBytes(Buffer.from('Universal Bob secret key'))
-    return Bob.fromSecretKey({ porterUri }, secretKey)
-  }
+export const makeTDecDecrypter = (configLabel: string, porterUri: string): tDecDecrypter => {
+  return new tDecDecrypter(
+    porterUri,
+    tDecConfig[configLabel]["policyEncryptingKey"],
+    tDecConfig[configLabel]["encryptedTreasureMapy"],
+    tDecConfig[configLabel]["publisherVerifyingKey"],
+    tDecConfig[configLabel]["decryptingKey"])
+};
+
+export const makeTDecEncrypter = (configLabel: string): Enrico => {
+  return new Enrico(
+    tDecConfig[configLabel]["policyEncryptingKey"],
+    tDecConfig[configLabel]["publisherVerifyingKey"])
+};

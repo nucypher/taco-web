a56d66d91e63c876114c8d41aa1da5fb9ec0bdcd
DAMON CIARELLI <gdamon@gmail.com>
2022-06-27 10:48:45 -0700

    beginning of python parity message kit/conditions concatenation

---
+++ src/core.ts
@@ -0,0 +1,28 @@
+import { Capsule, MessageKit as CoreMessageKit, PublicKey, SecretKey } from '@nucypher/nucypher-core';
+
+import { toBytes } from './utils';
+
+export class MessageKit extends CoreMessageKit{
+
+    private readonly delimiter: number
+    public readonly dcs: Array<Record<string, unknown>>;
+
+    constructor(decryptionConditions: Array<Record<string, unknown>>, policyEncryptingKey: PublicKey, plaintext: Uint8Array) {
+        super(
+            policyEncryptingKey, 
+            plaintext instanceof Uint8Array ? plaintext : toBytes(plaintext)
+        )
+
+        this.delimiter = 0xBC        
+        this.dcs = decryptionConditions
+
+    }
+
+    public toNewBytes = () => {
+        const conditionBytes = Buffer.from(JSON.stringify(this.dcs))
+        const delimiterBytes = Uint8Array.from([this.delimiter])
+        const mkBytes = super.toBytes()
+
+        return Buffer.from(new Uint8Array([...conditionBytes, ...delimiterBytes, ...mkBytes]))
+    }
+}

c7aa3132a167373827b1d267615a602c1ea091e2
mickey <mj@fortknoxster.com>
2022-08-08 16:51:08 +0200

    Exporting Porter and Keyring used by Alice

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { Alice } from './characters/alice';
 export { Bob, RemoteBob } from './characters/bob';
 export { Enrico } from './characters/enrico';
+export { Porter } from './characters/porter';
+export { Keyring } from './keyring';
 export {
   BlockchainPolicyParameters,
   EnactedPolicy,

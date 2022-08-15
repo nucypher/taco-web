bda89ad3eaf33b22d24a67bdda121ca29251a96a
DAMON CIARELLI <gdamon@gmail.com>
2022-08-15 13:45:53 -0700

    remove static config test

--- test/integration/tdec.test.ts
+++ test/integration/tdec.test.ts
@@ -22,20 +22,6 @@ import {
 describe('threshold decryption', () => {
   const plaintext = toBytes('plaintext-message');
 
-  it('encrypts and decrypts reencrypted message from static config', async () => {
-    const encrypter = await makeTDecEncrypter('james-ibex');
-    const decrypter = await makeTDecDecrypter(
-      'james-ibex',
-      'https://porter-ibex.nucypher.community'
-    );
-    const encryptedMessageKit = encrypter.encryptMessage(plaintext);
-
-    const bobPlaintext = await decrypter.retrieveAndDecrypt([
-      encryptedMessageKit,
-    ]);
-    expect(bobPlaintext[0]).toEqual(plaintext);
-  });
-
   it('encrypts and decrypts reencrypted message from dynamic config', async () => {
     const threshold = 3;
     const shares = 5;

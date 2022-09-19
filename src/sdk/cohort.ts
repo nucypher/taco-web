69ad4acc579b3b5771bf3c783cbc261d74bc28ab
James Campbell <james.campbell@tanti.org.uk>
2022-09-19 17:27:51 -0500

    Build create method for Cohort and add tests

--- src/sdk/cohort.ts
+++ src/sdk/cohort.ts
@@ -1,9 +1,51 @@
+import { PublicKey } from '@nucypher/nucypher-core';
+import { ChecksumAddress } from '../types';
+import { Porter } from '../characters/porter';
+
+export type Ursula = {
+    readonly checksumAddress: ChecksumAddress;
+    readonly uri: string;
+    readonly encryptingKey: PublicKey;
+  };
+interface CohortParameters {
+    porterUri: string;
+    threshold: number;
+    shares?: number;
+    include?: ChecksumAddress[];
+    exclude?: ChecksumAddress[];
+}
+
 export class Cohort {
-  
-    constructor() {
+    constructor(
+        public readonly ursulaAddresses: ChecksumAddress[],
+        public readonly threshold: number,
+    ) {
+        this.ursulaAddresses = ursulaAddresses;
+        this.threshold = threshold;
     }
 
-    public static create() {}
+    public static async create({
+        porterUri,
+        threshold,
+        shares = 0,
+        include = [],
+        exclude = []
+    }: CohortParameters) {
+        if (shares == 0 && include.length == 0) {
+            throw new TypeError('Shares is 0 and Include is an empty array');
+        }
+        if (shares == 0 && include.length > 0) {
+            shares = include.length;
+        }
+
+        const porter = new Porter(porterUri);
+        const ursulas = await porter.getUrsulas(
+            shares,
+            exclude,
+            include);
+        const ursulaAddresses = ursulas.map((ursula) => ursula.checksumAddress);
+        return new Cohort(ursulaAddresses, threshold);
+    }
   
     public static fromJson() {}
 
---
+++ test/unit/cohort.test.ts
@@ -0,0 +1,26 @@
+import { Cohort } from '../../src/sdk/cohort';
+
+import { mockUrsulas, mockGetUrsulas } from '../utils';
+
+describe('Cohort', () => {  
+    test('should throw an error if created without shares or cohort', async () => {
+      await expect(Cohort.create({
+          porterUri: "test uri",
+          threshold: 5
+        })).rejects.toThrow('Shares is 0 and Include is an empty array');
+    });
+
+    it('can create Cohort from list of ursulas', async () => {
+      const mockedUrsulas = mockUrsulas().slice(0, 5);
+      const getUrsulasSpy = mockGetUrsulas(mockedUrsulas);
+      const includeUrsulas = ["test string"];
+
+      const testCohort = await Cohort.create({
+        porterUri: 'https://porter-ibex.nucypher.community',
+        threshold: 5,
+        include: includeUrsulas
+      });
+
+      expect(getUrsulasSpy).toHaveBeenCalled();
+    });
+});

c577c26f6a41d201004b5e8e8a6704262384aaf9
James Campbell <james.campbell@tanti.org.uk>
2022-09-19 18:32:57 -0500

    Build initial Strategy based off cohort

--- src/sdk/strategy.ts
+++ src/sdk/strategy.ts
@@ -1,27 +1,65 @@
+import {
+    SecretKey,
+  } from '@nucypher/nucypher-core';
+import { ethers } from 'ethers';
+
+import { Cohort } from './cohort';
+import { Alice } from '../characters/alice';
+
 export class Strategy {
   
-    constructor() {
+  constructor(
+    cohort: Cohort,
+    startDate: Date,
+    endDate: Date,
+    alice: Alice
+  ) {
+  }
+
+  public static create(
+    cohort: Cohort,
+    startDate: Date,
+    endDate: Date,
+    porterUri: string,
+    provider: ethers.providers.Web3Provider,
+    aliceSecretKey?: SecretKey,
+    dkgAlice?: boolean
+  ) {
+    if (dkgAlice == true) {
+      throw new TypeError('DKG Alice is not yet implemented');
+    }
+    if (typeof(aliceSecretKey) == 'undefined') {
+      aliceSecretKey = SecretKey.random()
     }
 
-    public static create() {}
-  
-    public static fromJson() {}
+    const configuration = { porterUri };
+    const alice = Alice.fromSecretKey(configuration, aliceSecretKey, provider);
 
-    public static toJson() {}
-
-    public static deploy() {}
-
-    public static update() {}
-
-    public static revoke() {}
-
-    public static attachConditions() {}
-
-    public get encrypter(): any {
-        return this.encrypter;
+    return new Strategy(
+      cohort,
+      startDate,
+      endDate,
+      alice
+      )
     }
 
-    public get decrypter(): any {
-        return this.decrypter;
-    }
+  public static fromJson() {}
+
+  public static toJson() {}
+
+  public static deploy() {}
+
+  public static update() {}
+
+  public static revoke() {}
+
+  public static attachConditions() {}
+
+  public get encrypter(): any {
+      return this.encrypter;
+  }
+
+  public get decrypter(): any {
+      return this.decrypter;
+  }
 }

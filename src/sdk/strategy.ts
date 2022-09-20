db747106c502e32259802c34656e3e9f0ff4e0a3
James Campbell <james.campbell@tanti.org.uk>
2022-09-20 14:54:21 -0500

    Complete Strategy constructor and methods for Policy, Encrypter, and Decrypter

--- src/sdk/strategy.ts
+++ src/sdk/strategy.ts
@@ -1,14 +1,27 @@
 import { SecretKey } from '@nucypher/nucypher-core';
 import { ethers } from 'ethers';
 
+import { EnactedPolicy } from '../policies/policy';
 import { Alice } from '../characters/alice';
+import { Bob } from '../characters/bob';
+import { Enrico } from '../characters/enrico';
+import { tDecDecrypter } from '../characters/universal-bob';
 
 import { Cohort } from './cohort';
 
 export class Strategy {
-  constructor(cohort: Cohort, startDate: Date, endDate: Date, alice: Alice) {
-    throw new Error('Method not implemented.');
-  }
+  public policy?: EnactedPolicy;
+  public encrypter?: Enrico;
+  public decrypter?: tDecDecrypter;
+  constructor(
+    public cohort: Cohort,
+    public startDate: Date,
+    public endDate: Date,
+    private alice: Alice,
+    private bob: Bob,
+    private bobSecretKey: SecretKey,
+    public deployed: boolean
+  ) {}
 
   public static create(
     cohort: Cohort,
@@ -27,9 +40,19 @@ export class Strategy {
     }
 
     const configuration = { porterUri };
+    // DKG Alice can be created instead
     const alice = Alice.fromSecretKey(configuration, aliceSecretKey, provider);
-
-    return new Strategy(cohort, startDate, endDate, alice);
+    const bobSecretKey = SecretKey.random();
+    const bob = new Bob(configuration, bobSecretKey);
+    return new Strategy(
+      cohort,
+      startDate,
+      endDate,
+      alice,
+      bob,
+      bobSecretKey,
+      false
+    );
   }
 
   public static fromJson() {
@@ -40,12 +63,52 @@ export class Strategy {
     throw new Error('Method not implemented.');
   }
 
-  public static deploy() {
-    throw new Error('Method not implemented.');
+  public async deploy(label: string) {
+    const policyParams = {
+      bob: this.bob,
+      label,
+      threshold: this.cohort.threshold,
+      shares: this.cohort.ursulaAddresses.length,
+      startDate: this.startDate,
+      endDate: this.endDate,
+    };
+    this.policy = await this.alice.grant(
+      policyParams,
+      this.cohort.ursulaAddresses
+    );
+    this.deployed = true;
+    return this.policy;
   }
 
-  public static update() {
-    throw new Error('Method not implemented.');
+  public getEncrypter() {
+    if (this.deployed !== true) {
+      throw new Error('Policy has not yet been deployed.');
+    } else if (typeof this.policy == 'undefined') {
+      throw new Error('Policy has not yet been built.');
+    } else {
+      this.encrypter = new Enrico(
+        this.policy.policyKey,
+        this.alice.verifyingKey
+      );
+      return this.encrypter;
+    }
+  }
+
+  public getDecrypter() {
+    if (this.deployed !== true) {
+      throw new Error('Policy has not yet been deployed.');
+    } else if (typeof this.policy == 'undefined') {
+      throw new Error('Policy has not yet been built.');
+    } else {
+      this.decrypter = new tDecDecrypter(
+        this.cohort.porterUri,
+        this.policy.policyKey,
+        this.policy.encryptedTreasureMap,
+        this.alice.verifyingKey,
+        this.bobSecretKey,
+        this.bobSecretKey
+      );
+    }
   }
 
   public static revoke() {
@@ -54,13 +117,5 @@ export class Strategy {
 
   public static attachConditions() {
     throw new Error('Method not implemented.');
-  }
-
-  public get encrypter(): any {
-    return this.encrypter;
-  }
-
-  public get decrypter(): any {
-    return this.decrypter;
   }
 }

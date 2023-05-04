89a50de8c6dff3ff2dfef22f2724c0425f45daac
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-05-04 13:36:11 +0200

    handle missing contracts

--- src/agents/contracts.ts
+++ src/agents/contracts.ts
@@ -1,22 +1,22 @@
 import { ChainId, ChecksumAddress } from '../types';
 
 type Contracts = {
-  readonly SUBSCRIPTION_MANAGER: ChecksumAddress;
-  readonly COORDINATOR: ChecksumAddress;
+  readonly SUBSCRIPTION_MANAGER: ChecksumAddress | undefined;
+  readonly COORDINATOR: ChecksumAddress | undefined;
 };
 
 const POLYGON: Contracts = {
   SUBSCRIPTION_MANAGER: '0xB0194073421192F6Cf38d72c791Be8729721A0b3',
-  COORDINATOR: '0x0',
+  COORDINATOR: undefined,
 };
 
 const MUMBAI: Contracts = {
   SUBSCRIPTION_MANAGER: '0xb9015d7b35ce7c81dde38ef7136baa3b1044f313',
-  COORDINATOR: '0x0',
+  COORDINATOR: undefined,
 };
 
 const GOERLI: Contracts = {
-  SUBSCRIPTION_MANAGER: '0x0',
+  SUBSCRIPTION_MANAGER: undefined,
   COORDINATOR: '0x2cf19429168a0943992D8e7dE534E9b802C687B6',
 };
 
@@ -36,7 +36,11 @@ export const getContract = (
   if (!Object.keys(CONTRACTS[chainId as ChainId]).includes(contract)) {
     throw new Error(`No contract found for name: ${contract}`);
   }
-  return CONTRACTS[chainId as ChainId][contract];
+  const address = CONTRACTS[chainId as ChainId][contract];
+  if (!address) {
+    throw new Error(`No address found for contract: ${contract}`);
+  }
+  return address;
 };
 
 export const DEFAULT_WAIT_N_CONFIRMATIONS = 1;

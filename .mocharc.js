4e0d7b7a7eef219e2508f1ed7556ae1072869644
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-09-12 17:32:43 +0200

    chore: clean up package.json files

--- .mocharc.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = 'test'
-
-module.exports = {
-  require: ['ts-node/register/transpile-only'],
-  extension: ['ts'],
-  watchExtensions: ['ts'],
-  spec: ['test/**/*.test.ts'],
-  timeout: 300_000,
-}

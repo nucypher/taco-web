f2b57477458881b3e47935ac9ff69f5b8e17f048
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-09-13 17:24:40 +0200

    fix(test): fix missing export

--- packages/shared/src/conditions/index.ts
+++ packages/shared/src/conditions/index.ts
@@ -1,5 +1,10 @@
+// TODO: Do we want structured exports in @nucypher/shared?
 import * as base from './base';
 import * as predefined from './predefined';
+
+// TODO: Or do we want to export everything from the base and predefined modules?
+export * from './base';
+export * from './predefined';
 
 export {
   CompoundConditionType,

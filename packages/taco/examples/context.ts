8a024af0b9d0fb5fcc46dfe8ed120dbe719025cc
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-10-26 15:52:01 +0200

    chore(docs): add context param example

---
+++ packages/taco/examples/context.ts
@@ -0,0 +1,23 @@
+import { conditions } from '../src';
+import {CustomContextParam} from "../src/conditions";
+
+
+const ownsNFTRaw = new conditions.base.ContractCondition({
+  method: 'balanceOf',
+  parameters: [':userAddress'],
+  standardContractType: 'ERC721',
+  contractAddress: '0x1e988ba4692e52Bc50b375bcC8585b95c48AaD77',
+  chain: 5,
+  returnValueTest: {
+    index: 0,
+    comparator: '>',
+    value: ':selectedBalance',
+  },
+});
+console.assert(ownsNFTRaw.requiresSigner(), 'ContractCondition requires a signer');
+
+
+const customParameters: Record<string, CustomContextParam> = {
+  ':selectedBalance': 2,
+};
+console.log(customParameters);

dc60491e268413780cc4780b7aa2e7c7439080bc
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-11-07 09:30:49 +0100

    feature(conditions): context param can be any colon-prefixed string

--- packages/taco/src/conditions/base/shared.ts
+++ packages/taco/src/conditions/base/shared.ts
@@ -1,12 +1,9 @@
 import { z } from 'zod';
 
-import {
-  CONTEXT_PARAM_REGEXP,
-  ETH_ADDRESS_REGEXP,
-  USER_ADDRESS_PARAM,
-} from '../const';
+import { ETH_ADDRESS_REGEXP, USER_ADDRESS_PARAM } from '../const';
+import { CONTEXT_PARAM_PREFIX } from '../context/context';
 
-export const ContextParamSchema = z.string().regex(CONTEXT_PARAM_REGEXP);
+export const ContextParamSchema = z.string().startsWith(CONTEXT_PARAM_PREFIX);
 export const ParamSchema = z.union([z.number(), z.string(), z.boolean()]);
 export const ParamOrContextParamSchema = z.union([
   ParamSchema,
--- packages/taco/src/conditions/const.ts
+++ packages/taco/src/conditions/const.ts
@@ -4,8 +4,6 @@ export const USER_ADDRESS_PARAM = ':userAddress';
 
 export const ETH_ADDRESS_REGEXP = new RegExp('^0x[a-fA-F0-9]{40}$');
 
-export const CONTEXT_PARAM_REGEXP = new RegExp('^:[a-zA-Z0-9]+$');
-
 export const SUPPORTED_CHAIN_IDS = [
   ChainId.POLYGON,
   ChainId.MUMBAI,

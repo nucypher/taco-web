a9e960181fbe4d07fc1fc7ab061c1d0162790419
Piotr Roslaniec <p.roslaniec@gmail.com>
2024-01-09 12:27:17 +0100

    chore(demos): use testnet in demos

--- demos/taco-demo/src/config.ts
+++ demos/taco-demo/src/config.ts
@@ -1,7 +1,4 @@
 import { domains } from '@nucypher/taco';
 
-export const DEFAULT_RITUAL_ID =
-  (process.env.DEFAULT_RITUAL_ID && parseInt(process.env.DEFAULT_RITUAL_ID)) ||
-  24;
-
-export const DEFAULT_DOMAIN = process.env.DEFAULT_DOMAIN || domains.DEVNET;
+export const DEFAULT_RITUAL_ID = parseInt(process.env.DEFAULT_RITUAL_ID || '5');
+export const DEFAULT_DOMAIN = process.env.DEFAULT_DOMAIN || domains.TESTNET;

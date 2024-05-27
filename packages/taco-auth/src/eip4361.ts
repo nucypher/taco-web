8eadb4423dd901774713579199458036a5ad3182
Manuel Montenegro <manuel@nucypher.com>
2024-05-27 19:37:42 +0200

    Use TypedSignature format as return of SiweMessage

--- packages/taco-auth/src/eip4361.ts
+++ packages/taco-auth/src/eip4361.ts
@@ -2,6 +2,7 @@ import { SiweMessage } from '@didtools/cacao';
 import { ethers } from 'ethers';
 
 import { LocalStorage } from './storage';
+import { TypedSignature } from './typedSignature';
 
 export class EIP4361SignatureProvider {
   private readonly storage: LocalStorage;
@@ -13,36 +14,29 @@ export class EIP4361SignatureProvider {
     this.storage = new LocalStorage();
   }
 
-  public async getOrCreateSiweMessage(
-    domain: string,
-    version: string,
-    nonce: string,
-    uri: string,
-  ): Promise<string> {
+  public async getOrCreateSiweMessage(): Promise<TypedSignature> {
     const address = await this.signer.getAddress();
     const storageKey = `eth-signin-message-${address}`;
 
     // If we have a message in localStorage, return it
     const maybeMessage = this.storage.getItem(storageKey);
     if (maybeMessage) {
-      return maybeMessage;
+      return JSON.parse(maybeMessage);
     }
 
     // If at this point we didn't return, we need to create a new message
-    const message = await this.createSiweMessage(domain, version, nonce, uri);
-    this.storage.setItem(storageKey, message);
-    return message;
+    const typedSignature = await this.createSiweMessage();
+    this.storage.setItem(storageKey, JSON.stringify(typedSignature));
+    return typedSignature;
   }
 
-  private async createSiweMessage(
-    domain: string,
-    version: string,
-    nonce: string,
-    uri: string,
-  ): Promise<string> {
-    const chainId = (await this.provider.getNetwork()).chainId;
+  private async createSiweMessage(): Promise<TypedSignature> {
     const address = await this.signer.getAddress();
-
+    const domain = 'https://login.xyz';
+    const version = '1';
+    const nonce = '0';
+    const uri = 'did:key:z6MkrBdNdwUPnXDVD1DCxedzVVBpaGi8aSmoXFAeKNgtAer8';
+    const chainId = (await this.provider.getNetwork()).chainId;
     const siweMessage = new SiweMessage({
       domain,
       address,
@@ -53,6 +47,9 @@ export class EIP4361SignatureProvider {
       chainId: chainId.toString(),
     });
 
-    return siweMessage.toMessage();
+    const scheme = 'SIWE';
+    const signature = await this.signer.signMessage(siweMessage.toMessage());
+
+    return { signature, address, scheme, typedData: siweMessage };
   }
 }

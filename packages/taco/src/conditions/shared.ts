af9948a4408776c2d6c22f17ce8ea7cdbcb2fc14
derekpierre <derek.pierre@gmail.com>
2024-05-30 15:04:45 -0400

    Update userAddressSchema to accomodate additional userAddress auth-specific values.

--- packages/taco/src/conditions/shared.ts
+++ packages/taco/src/conditions/shared.ts
@@ -4,6 +4,8 @@ import {
   CONTEXT_PARAM_PREFIX,
   CONTEXT_PARAM_REGEXP,
   ETH_ADDRESS_REGEXP,
+  USER_ADDRESS_PARAM_EIP712,
+  USER_ADDRESS_PARAM_EIP4361,
   USER_ADDRESS_PARAM_DEFAULT,
 } from './const';
 
@@ -36,7 +38,8 @@ export const returnValueTestSchema = z.object({
 export type ReturnValueTestProps = z.infer<typeof returnValueTestSchema>;
 
 const EthAddressSchema = z.string().regex(ETH_ADDRESS_REGEXP);
-const UserAddressSchema = z.literal(USER_ADDRESS_PARAM_DEFAULT);
+
+const UserAddressSchema = z.enum([USER_ADDRESS_PARAM_EIP712, USER_ADDRESS_PARAM_EIP4361, USER_ADDRESS_PARAM_DEFAULT]);
 export const EthAddressOrUserAddressSchema = z.union([
   EthAddressSchema,
   UserAddressSchema,

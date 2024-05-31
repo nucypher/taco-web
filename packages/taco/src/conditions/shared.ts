812852effd31c8767a73a15faab6fe99a33c7f42
derekpierre <derek.pierre@gmail.com>
2024-05-30 22:22:33 -0400

    Fix/improve tests for taco-auth/context. Run linter.

--- packages/taco/src/conditions/shared.ts
+++ packages/taco/src/conditions/shared.ts
@@ -4,9 +4,9 @@ import {
   CONTEXT_PARAM_PREFIX,
   CONTEXT_PARAM_REGEXP,
   ETH_ADDRESS_REGEXP,
-  USER_ADDRESS_PARAM_EIP712,
-  USER_ADDRESS_PARAM_EIP4361,
   USER_ADDRESS_PARAM_DEFAULT,
+  USER_ADDRESS_PARAM_EIP4361,
+  USER_ADDRESS_PARAM_EIP712,
 } from './const';
 
 export const contextParamSchema = z.string().regex(CONTEXT_PARAM_REGEXP);
@@ -39,7 +39,11 @@ export type ReturnValueTestProps = z.infer<typeof returnValueTestSchema>;
 
 const EthAddressSchema = z.string().regex(ETH_ADDRESS_REGEXP);
 
-const UserAddressSchema = z.enum([USER_ADDRESS_PARAM_EIP712, USER_ADDRESS_PARAM_EIP4361, USER_ADDRESS_PARAM_DEFAULT]);
+const UserAddressSchema = z.enum([
+  USER_ADDRESS_PARAM_EIP712,
+  USER_ADDRESS_PARAM_EIP4361,
+  USER_ADDRESS_PARAM_DEFAULT,
+]);
 export const EthAddressOrUserAddressSchema = z.union([
   EthAddressSchema,
   UserAddressSchema,

c4f09456410b24beffe7a128f9dff5730f2e5eb5
derekpierre <derek.pierre@gmail.com>
2024-05-30 22:20:17 -0400

    typedData entry for EIP4361 should be a string. Fix the URI used for EIP4361 message to be properly formed - it caused an invalid SIWE message to be created.

--- packages/taco-auth/src/types.ts
+++ packages/taco-auth/src/types.ts
@@ -1,10 +1,8 @@
-import type { SiweMessage } from 'siwe';
-
 import { FormattedEip712 } from './eip712';
 
 export interface TypedSignature {
   signature: string;
   address: string;
   scheme: 'EIP712' | 'EIP4361';
-  typedData: FormattedEip712 | SiweMessage;
+  typedData: FormattedEip712 | string;
 }

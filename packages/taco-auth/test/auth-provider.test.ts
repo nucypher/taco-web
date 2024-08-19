09437cc77c6be7806e8cbef59bc0c76bc47b48fd
derekpierre <derek.pierre@gmail.com>
2024-08-19 12:31:01 -0400

    Add test for EIP4361AuthProvider storage expiry.

--- packages/taco-auth/test/auth-provider.test.ts
+++ packages/taco-auth/test/auth-provider.test.ts
@@ -5,7 +5,7 @@ import {
   TEST_SIWE_PARAMS,
 } from '@nucypher/test-utils';
 import { SiweMessage } from 'siwe';
-import { describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { EIP4361AuthProvider } from '../src/providers';
 import { EIP4361TypedDataSchema } from '../src/providers/eip4361/common';
@@ -50,5 +50,52 @@ describe('auth provider', () => {
     expect(() =>
       EIP4361TypedDataSchema.parse(typedSignature.typedData),
     ).toThrow();
+  });
+});
+
+describe('auth provider caching', () => {
+  beforeEach(() => {
+    // tell vitest we use mocked time
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    // restoring date after each test run
+    vi.useRealTimers();
+  });
+
+  const provider = fakeProvider(bobSecretKeyBytes);
+  const signer = fakeSigner(bobSecretKeyBytes);
+  const eip4361Provider = new EIP4361AuthProvider(
+    provider,
+    signer,
+    TEST_SIWE_PARAMS,
+  );
+
+  it('caches auth signature, but regenerates when expired', async () => {
+    const createAuthSignatureSpy = vi.spyOn(
+      eip4361Provider,
+      'createSIWEAuthMessage',
+    );
+
+    const typedSignature = await eip4361Provider.getOrCreateAuthSignature();
+    expect(createAuthSignatureSpy).toHaveBeenCalledTimes(1);
+
+    const typedSignatureSecondCall =
+      await eip4361Provider.getOrCreateAuthSignature();
+    // auth signature not expired, so spy is not called a 2nd time
+    expect(createAuthSignatureSpy).toHaveBeenCalledTimes(1);
+    expect(typedSignatureSecondCall).toEqual(typedSignature);
+
+    // time travel to 2h:1m in the future; auth signature is now expired
+    const now = new Date();
+    now.setHours(now.getHours() + 2, now.getMinutes() + 1);
+    vi.setSystemTime(now);
+
+    const typedSignatureThirdCall =
+      await eip4361Provider.getOrCreateAuthSignature();
+    // auth signature is now expired, so spy is called a 2nd time
+    expect(createAuthSignatureSpy).toHaveBeenCalledTimes(2);
+    expect(typedSignatureThirdCall).not.toEqual(typedSignature);
   });
 });

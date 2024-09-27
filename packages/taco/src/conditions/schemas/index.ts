ae4e402d2ecbbf8b81abae398b94c595ba9bc13c
derekpierre <derek.pierre@gmail.com>
2024-09-27 10:56:24 -0400

    Use schemas from schemas folder, and re-export relevant types etc.

--- packages/taco/src/conditions/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-export * as common from './common';
-export * as compound from './compound';
-export * as context from './context';
-export * as contract from './contract';
-export * as returnValueTest from './return-value-test';
-export * as rpc from './rpc';
-export * as sequential from './sequential';
-export * as time from './time';

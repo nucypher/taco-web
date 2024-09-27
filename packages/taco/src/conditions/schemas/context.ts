02ab6c02b032e416917fd0a5128ea3d829aed72a
derekpierre <derek.pierre@gmail.com>
2024-09-27 10:55:00 -0400

    Add schemas folder to house all zod schemas for conditions, context, etc. Add common single schema that encompasses all possible condition schemas.

---
+++ packages/taco/src/conditions/schemas/context.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+import { CONTEXT_PARAM_REGEXP } from '../const';
+
+import { plainStringSchema } from './common';
+
+export const contextParamSchema = z.string().regex(CONTEXT_PARAM_REGEXP);
+
+const paramSchema = z.union([plainStringSchema, z.boolean(), z.number()]);
+
+export const paramOrContextParamSchema: z.ZodSchema = z.union([
+  paramSchema,
+  contextParamSchema,
+  z.lazy(() => z.array(paramOrContextParamSchema)),
+]);

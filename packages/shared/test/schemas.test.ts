f6084dfaa4965ac94d4104fdcaa8dc7dc09f098c
David Núñez <david@nucypher.com>
2024-10-18 13:28:30 +0200

    test: improve eth address schema unit tests

--- packages/shared/test/schemas.test.ts
+++ packages/shared/test/schemas.test.ts
@@ -8,8 +8,23 @@ describe('ethereum address schema', () => {
     EthAddressSchema.parse(validAddress);
   });
 
-  it('should reject invalid ethereum address', () => {
+  it('should accept unchecksummed ethereum address', () => {
+    const validAddress = '0x0123456789abcdefedcb0123456789abcdefedcb';
+    EthAddressSchema.parse(validAddress);
+  });
+
+  it('should accept checksummed ethereum address', () => {
+    const validAddress = '0x0123456789aBcDeFEdCb0123456789abcdEfeDcb';
+    EthAddressSchema.parse(validAddress);
+  });
+
+  it('should reject invalid ethereum address (shorter)', () => {
     const invalidAddress = '0x123456789012345678901234567890123456789';
+    expect(() => EthAddressSchema.parse(invalidAddress)).toThrow();
+  });
+
+  it('should reject invalid ethereum address (longer)', () => {
+    const invalidAddress = '0x12345678901234567890123456789012345678901';
     expect(() => EthAddressSchema.parse(invalidAddress)).toThrow();
   });
 });

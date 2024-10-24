f971fead495334653c0a4b7de4157605aa2afccb
David Núñez <david@nucypher.com>
2024-10-24 18:38:15 +0200

    feat: Add error message to BlockHash schema validation
    Co-authored-by: Derek Pierre <derek.pierre@gmail.com>

--- packages/shared/src/schemas.ts
+++ packages/shared/src/schemas.ts
@@ -15,7 +15,7 @@ export const EthAddressSchema = z
 
 const BLOCK_HASH_REGEXP = new RegExp('^0x[a-fA-F0-9]{64}$');
 const BlockNumber = z.number().int().nonnegative();
-const BlockHash = z.string().regex(BLOCK_HASH_REGEXP);
+const BlockHash = z.string().regex(BLOCK_HASH_REGEXP, 'Invalid block hash');
 const BlockTag = z.enum(['earliest', 'finalized', 'safe', 'latest', 'pending']);
 
 export const BlockIdentifierSchema = z.union([BlockNumber, BlockHash, BlockTag]);

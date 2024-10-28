521d56ebb53b23f7ace939e8e6e678ed30e8a02f
derekpierre <derek.pierre@gmail.com>
2024-10-28 09:35:19 -0400

    Add tests for authorization token property for JsonApiCondition.

--- packages/taco/test/conditions/base/json.test.ts
+++ packages/taco/test/conditions/base/json.test.ts
@@ -39,6 +39,39 @@ describe('JsonApiCondition', () => {
       });
     });
 
+    describe('authorizationToken', () => {
+      it('accepts context variable', () => {
+        const result = JsonApiCondition.validate(jsonApiConditionSchema, {
+          ...testJsonApiConditionObj,
+          authorizationToken: ':authToken',
+        });
+        expect(result.error).toBeUndefined();
+        expect(result.data).toEqual({
+          ...testJsonApiConditionObj,
+          authorizationToken: ':authToken',
+        });
+      });
+      it.each([
+        'authToken',
+        'ABCDEF1234567890',
+        ':authToken?',
+        '$:authToken',
+        ':auth-Token',
+      ])('rejects invalid context variable', (contextVar) => {
+        const result = JsonApiCondition.validate(jsonApiConditionSchema, {
+          ...testJsonApiConditionObj,
+          authorizationToken: `${contextVar}`,
+        });
+        expect(result.error).toBeDefined();
+        expect(result.data).toBeUndefined();
+        expect(result.error?.format()).toMatchObject({
+          authorizationToken: {
+            _errors: ['Invalid'],
+          },
+        });
+      });
+    });
+
     describe('parameters', () => {
       it('accepts conditions without query path', () => {
         const { query, ...noQueryObj } = testJsonApiConditionObj;

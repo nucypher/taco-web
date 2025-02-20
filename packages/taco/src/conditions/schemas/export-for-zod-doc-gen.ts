15897f9748ad4744a9fbc5e4bf16392bb10cb573
Muhammad Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2025-02-20 18:45:02 +0100

    generate md file for zod schemas using modifed version of zod2md
    Signed-off-by: Muhammad Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>

---
+++ packages/taco/src/conditions/schemas/export-for-zod-doc-gen.ts
@@ -0,0 +1,13 @@
+
+export * from './utils';
+export * from './common';
+export * from './compound';
+export * from './context';
+export * from './contract';
+export * from './if-then-else';
+export * from './json-api';
+export * from './json-rpc';
+export * from './return-value-test';
+export * from './rpc';
+export * from './sequential';
+export * from './time';

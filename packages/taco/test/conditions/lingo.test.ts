ffcc9c15f42f19c6f592d6893b2a445512aa1b00
derekpierre <derek.pierre@gmail.com>
2025-03-19 09:46:08 -0400

    Add test for using large numbers in conditions as parameters or return value test. Tests the range of [-2^255, 2^256-1].

--- packages/taco/test/conditions/lingo.test.ts
+++ packages/taco/test/conditions/lingo.test.ts
@@ -1,6 +1,7 @@
 import { TEST_CHAIN_ID } from '@nucypher/test-utils';
 import { describe, expect, it } from 'vitest';
 
+import { ContractCondition } from '../../src/conditions/base/contract';
 import { ConditionExpression } from '../../src/conditions/condition-expr';
 import {
   testJsonApiConditionObj,
@@ -97,5 +98,84 @@ describe('check that valid lingo in python is valid in typescript', () => {
     const conditionExpr = ConditionExpression.fromObj(conditionExprJSON);
     expect(conditionExpr.toObj()).toBeDefined();
     expect(conditionExpr.condition.toObj()).toEqual(conditionProps);
+  });
+});
+
+describe('check large numbers', () => {
+  it('checking uint256 max', () => {
+    const uint256ContractConditionProps = {
+      conditionType: 'contract',
+      chain: TEST_CHAIN_ID,
+      method: 'method',
+      parameters: [
+        // eslint-disable-next-line @typescript-eslint/no-loss-of-precision
+        115792089237316195423570985008687907853269984665640564039457584007913129639935,
+      ],
+      contractAddress: '0xA1bd3630a13D54EDF7320412B5C9F289230D260d',
+      functionAbi: {
+        type: 'function',
+        name: 'someMethod',
+        stateMutability: 'view',
+        inputs: [
+          {
+            name: '_value',
+            type: 'uint256',
+            internalType: 'uint256',
+          },
+        ],
+        outputs: [{ name: '', type: 'uint256', internalType: 'uint256' }],
+      },
+      returnValueTest: {
+        comparator: '==',
+        // eslint-disable-next-line @typescript-eslint/no-loss-of-precision
+        value: 115792089237316195423570985008687907853269984665640564039457584007913129639935,
+      },
+    };
+
+    const contractCondition = new ContractCondition(
+      uint256ContractConditionProps,
+    );
+    const conditionExpr = new ConditionExpression(contractCondition);
+    expect(conditionExpr.toJson()).toContain(
+      '115792089237316195423570985008687907853269984665640564039457584007913129639935',
+    );
+  });
+
+  it('checking int256 min', () => {
+    const int256ContractConditionProps = {
+      conditionType: 'contract',
+      chain: TEST_CHAIN_ID,
+      method: 'method',
+      parameters: [
+        -57896044618658097711785492504343953926634992332820282019728792003956564819968,
+      ],
+      contractAddress: '0xA1bd3630a13D54EDF7320412B5C9F289230D260d',
+      functionAbi: {
+        type: 'function',
+        name: 'someMethod',
+        stateMutability: 'view',
+        inputs: [
+          {
+            name: '_value',
+            type: 'int256',
+            internalType: 'int256',
+          },
+        ],
+        outputs: [{ name: '', type: 'int256', internalType: 'int256' }],
+      },
+      returnValueTest: {
+        comparator: '==',
+        value:
+          -57896044618658097711785492504343953926634992332820282019728792003956564819968,
+      },
+    };
+
+    const contractCondition = new ContractCondition(
+      int256ContractConditionProps,
+    );
+    const conditionExpr = new ConditionExpression(contractCondition);
+    expect(conditionExpr.toJson()).toContain(
+      '-5789604461865809771178549250434395392663499233282028201972879200395',
+    );
   });
 });

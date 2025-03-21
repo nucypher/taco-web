03f1def2177c2cebba3087374441a4daed243c77
Muhammad Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2025-03-21 17:21:34 +0100

    enhance integration test

--- packages/taco/integration-test/encrypt-decrypt.test.ts
+++ packages/taco/integration-test/encrypt-decrypt.test.ts
@@ -14,6 +14,7 @@ import {
   initialize,
   ThresholdMessageKit,
 } from '../src';
+import { CompoundCondition } from '../src/conditions/compound-condition';
 
 dotenv.config({ path: 'integration-test/.env' });
 
@@ -69,72 +70,96 @@ describe.skip('Taco Encrypt/Decrypt Integration Test', () => {
     }
   });
 
-  test.each([
-    0,
-    1000000000000000000000, // 1000 ETH/PLY
-    BigInt(10000000000000000), // 0.01 ETH/PLY
-  ])(
-    'should encrypt and decrypt a message with condition value %s',
-    async (value) => {
-      // Create test message
-      const messageString = 'This is a secret 🤐';
-      const message = toBytes(messageString);
+  test('should encrypt and decrypt a message with large condition values %s', async (value) => {
+    // Create test message
+    const messageString = 'This is a secret 🤐';
+    const message = toBytes(messageString);
 
-      // Create condition
-      const hasPositiveBalance = new conditions.base.rpc.RpcCondition({
+    // Create conditions
+    const hasPositiveBalance = new conditions.base.rpc.RpcCondition({
+      chain: chainId,
+      method: 'eth_getBalance',
+      parameters: [':userAddress', 'latest'],
+      returnValueTest: {
+        comparator: '>=',
+        value: 0,
+      },
+    });
+
+    const balanceLessThanMaxUint = new conditions.base.rpc.RpcCondition({
+      chain: chainId,
+      method: 'eth_getBalance',
+      parameters: [':userAddress', 'latest'],
+      returnValueTest: {
+        comparator: '<',
+        // max uint256
+        value: BigInt(
+          '115792089237316195423570985008687907853269984665640564039457584007913129639935',
+        ),
+      },
+    });
+
+    const balanceLessThanLargeFloatNumber =
+      new conditions.base.rpc.RpcCondition({
         chain: chainId,
         method: 'eth_getBalance',
         parameters: [':userAddress', 'latest'],
         returnValueTest: {
-          comparator: '>=',
-          value,
+          comparator: '<',
+          // 1000 ETH/PLY (this number is more than the safe max integer in javascript for this javascript handle it as a float)
+          value: 1000000000000000000000,
         },
       });
 
-      // Encrypt message
-      const messageKit = await encrypt(
-        provider,
-        domain,
-        message,
-        hasPositiveBalance,
-        ritualId,
-        encryptorSigner,
+    const compoundCondition = CompoundCondition.and([
+      hasPositiveBalance,
+      balanceLessThanMaxUint,
+      balanceLessThanLargeFloatNumber,
+    ]);
+
+    // Encrypt message
+    const messageKit = await encrypt(
+      provider,
+      domain,
+      message,
+      compoundCondition,
+      ritualId,
+      encryptorSigner,
+    );
+
+    const encryptedBytes = messageKit.toBytes();
+
+    // Prepare for decryption
+    const messageKitFromBytes = ThresholdMessageKit.fromBytes(encryptedBytes);
+    const conditionContext =
+      conditions.context.ConditionContext.fromMessageKit(messageKitFromBytes);
+
+    // Add auth provider for condition
+    if (
+      conditionContext.requestedContextParameters.has(
+        USER_ADDRESS_PARAM_DEFAULT,
+      )
+    ) {
+      const authProvider = new EIP4361AuthProvider(provider, consumerSigner, {
+        domain: 'localhost',
+        uri: 'http://localhost:3000',
+      });
+      conditionContext.addAuthProvider(
+        USER_ADDRESS_PARAM_DEFAULT,
+        authProvider,
       );
+    }
 
-      const encryptedBytes = messageKit.toBytes();
+    // Decrypt message
+    const decryptedBytes = await decrypt(
+      provider,
+      domain,
+      messageKitFromBytes,
+      conditionContext,
+    );
+    const decryptedMessageString = fromBytes(decryptedBytes);
 
-      // Prepare for decryption
-      const messageKitFromBytes = ThresholdMessageKit.fromBytes(encryptedBytes);
-      const conditionContext =
-        conditions.context.ConditionContext.fromMessageKit(messageKitFromBytes);
-
-      // Add auth provider for condition
-      if (
-        conditionContext.requestedContextParameters.has(
-          USER_ADDRESS_PARAM_DEFAULT,
-        )
-      ) {
-        const authProvider = new EIP4361AuthProvider(provider, consumerSigner, {
-          domain: 'localhost',
-          uri: 'http://localhost:3000',
-        });
-        conditionContext.addAuthProvider(
-          USER_ADDRESS_PARAM_DEFAULT,
-          authProvider,
-        );
-      }
-
-      // Decrypt message
-      const decryptedBytes = await decrypt(
-        provider,
-        domain,
-        messageKitFromBytes,
-        conditionContext,
-      );
-      const decryptedMessageString = fromBytes(decryptedBytes);
-
-      // Verify decryption
-      expect(decryptedMessageString).toEqual(messageString);
-    },
-  );
+    // Verify decryption
+    expect(decryptedMessageString).toEqual(messageString);
+  });
 });

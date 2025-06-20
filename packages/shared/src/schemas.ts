5a01b5da137674502c0bdb5b5ddfec481d7f14fd
Muhammad Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2025-06-20 23:01:37 +0200

    add and use EthAddressSchemaStrict and few other small edits

--- packages/shared/src/schemas.ts
+++ packages/shared/src/schemas.ts
@@ -9,9 +9,25 @@ const isAddress = (address: string) => {
   }
 };
 
+const isAddressStrict = (address: string) => {
+  try {
+    // Verify the address was checksummed to prevent passing an address that has a typo
+    return address === ethers.utils.getAddress(address);
+  } catch (e) {
+    // If any error occurs during validation, fail the validation
+    return false;
+  }
+};
+
 export const EthAddressSchema = z
   .string()
   .refine(isAddress, { message: 'Invalid Ethereum address' });
+
+export const EthAddressSchemaStrict = z
+  .string()
+  .refine(isAddressStrict, {
+    message: 'Invalid Ethereum address - it must be valid and checksummed',
+  });
 
 const BLOCK_HASH_REGEXP = new RegExp('^0x[a-fA-F0-9]{64}$');
 const BlockNumber = z.number().int().nonnegative();

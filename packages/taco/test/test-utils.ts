7c80207cf9ed79e32403f7782907bd0a88f28e20
derekpierre <derek.pierre@gmail.com>
2025-06-25 16:44:54 -0400

    Improve validation of abi calls including processing of solidity types, and parameter and tuple indices. Add corresponding tests.

--- packages/taco/test/test-utils.ts
+++ packages/taco/test/test-utils.ts
@@ -365,7 +365,7 @@ export const testSigningAttributeConditionObj: SigningObjectAttributeConditionPr
     },
   };
 
-// lots of nested abi calls
+// some nested abi calls
 export const testSigningObjectAbiAttributeConditionObj: SigningObjectAbiAttributeConditionProps =
   {
     conditionType: SigningObjectAbiAttributeConditionType,

bc1c5aba16bb2971441d153bbaf8b15a81a9ffda
James Campbell <james.campbell@tanti.org.uk>
2025-06-27 11:05:47 +0200

    Rename Signingcoordinator to signing-coordinator

--- packages/shared/src/contracts/agents/index.ts
+++ packages/shared/src/contracts/agents/index.ts
@@ -1,4 +1,4 @@
 export * from './coordinator';
 export * from './global-allow-list';
 export * from './subscription-manager';
-export * from './signingCoordinator';
+export * from './signing-coordinator';
